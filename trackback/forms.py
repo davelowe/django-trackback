ef4e3affb5bd8985506a4df10f6b2f10f29d2d46
arne@rcs4u.de <arne@rcs4u.de@7caa84ed-3b54-0410-bb33-ff8611a58c76>
2009-04-25 20:29:11 +0000

    in r11 the auto_now_add attribute of the submit_date field was replaced by overwriting the model's save() method. this exposed the date field to the Trackback ModelForm and yielded to validation errors. Excluding the submit_date field from the form should fix this problem. Thanks to mikeshoup for reporting this issue (#5).
    git-svn-id: http://django-trackback.googlecode.com/svn/trunk@14 7caa84ed-3b54-0410-bb33-ff8611a58c76

--- trackback/forms.py
+++ trackback/forms.py
@@ -5,4 +5,4 @@ from trackback.models import Trackback
 class TrackbackForm(forms.ModelForm):
     class Meta:
         model = Trackback
-        exclude = ('content_type', 'object_id', 'content_object', 'remote_ip', 'site',)
+        exclude = ('content_type', 'object_id', 'content_object', 'remote_ip', 'site', 'submit_date')
