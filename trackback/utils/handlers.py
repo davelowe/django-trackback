dce368f4a5916764a1459833d949369bde569d48
Dave Lowe <mahalowe@gmail.com>
2012-03-20 16:00:42 -0500

    Update URL matching regex

--- trackback/utils/handlers.py
+++ trackback/utils/handlers.py
@@ -1,20 +1,22 @@
 import re
 from trackback.utils.send import send_trackback as send_tb, send_pingback as send_pb
+from django.conf import settings
 from django.contrib.sites.models import Site
 
-URL_RE = re.compile(r'http://[^\ ]+', re.IGNORECASE) # TODO: this is simple+stupid
+URL_RE = re.compile(r'(https?://)?([\w]+\.)([\w]+\.)?(\.?[\w]{2,})([\/\d\w#\!]+?)?(?:\s)', re.IGNORECASE)
 
 
 def send_trackback(instance, **kwargs):
     """
     Default signal handler for sending trackbacks.
-    
-    Assumes that the ``instance`` has an attribute named 
+
+    Assumes that the ``instance`` has an attribute named
     ``trackback_content_field_name``, which contains the name of the
     model-field, which should be searched for urls to trackback.
-    
+
     """
-    content = getattr(instance, instance.trackback_content_field_name)
+    field_name = getattr(settings, "TRACKBACK_CONTENT_FIELD_NAME", {}).get(instance.__class__.__name__)
+    content = getattr(instance, field_name or instance.trackback_content_field_name)
     urls = URL_RE.findall(content)
     data = {}
     site = Site.objects.get_current()
@@ -22,21 +24,20 @@ def send_trackback(instance, **kwargs):
     data['title'] = unicode(instance)
     data['blog_name'] = site.name
     data['excerpt'] = content[:100]
-    
+
     for url in urls:
         #print "trackbacking %s" % url
-        send_tb(url, data,) # fail_silently=False)
-    
-    
+        send_tb(url, data,)  # fail_silently=False)
 
 
 def send_pingback(instance, **kwargs):
     """
     Default signal handler for sending pingbacks
-    
+
     """
-    content = getattr(instance, instance.trackback_content_field_name)
+    field_name = getattr(settings, "TRACKBACK_CONTENT_FIELD_NAME", {}).get(instance.__class__.__name__)
+    content = getattr(instance, field_name or instance.trackback_content_field_name)
     urls = URL_RE.findall(content)
     for url in urls:
         #print "pingbacking %s" % url
-        send_pb(instance.get_absolute_url(), url,)# fail_silently=False)
+        send_pb(instance.get_absolute_url(), url,)  # fail_silently=False)
